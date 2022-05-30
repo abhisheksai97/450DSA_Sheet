b2659eb1dfccf6a36866007bbc8ba790c11f7bfa
Abhishek Saini <99014292+abhisheksai97@users.noreply.github.com>
2022-05-30 16:21:50 +0530

    Create 177.Level Order -Traversal.cpp

---
+++ Tree's Question/177.Level Order -Traversal.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    {
+       vector<int> v;
+       queue<Node*> q;
+       q.push(node);
+       while(!q.empty()){
+           Node * temp=q.front();
+           q.pop();
+               v.push_back(temp->data);
+               if(temp->left){
+                   q.push(temp->left);
+               }
+               if(temp->right){
+                   q.push(temp->right);
+               }
+           
+       }
+       return v;
+    }
+};
